d2ac2a87b4e0b49009c294c5116f40d987ca4677
victorkolb <violetmonox@gmail.com>
2018-11-02 11:18:19 +0300

    old shiba api

--- src/getDog.js
+++ src/getDog.js
@@ -4,11 +4,15 @@ const dogUrls = {
   corgi: "https://dog.ceo/api/breed/corgi/cardigan/images/random",
   toy: "https://dog.ceo/api/breed/terrier/toy/images/random",
   pug: "https://dog.ceo/api/breed/pug/images/random",
-  shiba: "https://dog.ceo/api/breed/shiba/images/random",
+  shiba: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false",
 };
 
 export async function getDog(type) {
   console.log(type);
   const data = await response(dogUrls[type]);
-  return data.message.replace(/\\/g, "");
+  if (type === "shiba") {
+    return data[0];
+  } else {
+    return data.message.replace(/\\/g, "");
+  }
 }
