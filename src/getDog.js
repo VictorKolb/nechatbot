44f101f8bde64abecd7507d0d813297d22c21a65
victorkolb <violetmonox@gmail.com>
2018-12-27 18:22:52 +0300

    хаски

--- src/getDog.js
+++ src/getDog.js
@@ -3,7 +3,7 @@ import response from "response";
 const dogUrls = {
   corgi: "https://dog.ceo/api/breed/corgi/cardigan/images/random",
   toy: "https://dog.ceo/api/breed/terrier/toy/images/random",
-  husky: "https://dog.ceo/api/breed/terrier/husky/images/random",
+  husky: "https://dog.ceo/api/breed/husky/images/random",
   pug: "https://dog.ceo/api/breed/pug/images/random",
   shiba: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false",
   randomDog: "https://dog.ceo/api/breeds/image/random",
