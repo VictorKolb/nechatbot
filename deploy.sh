e7e3a97bcc5adddb503b476374b92bc8d6cb883d
victorkolb <violetmonox@gmail.com>
2019-09-30 21:21:06 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-rsync -av -e ssh --exclude="node_modules" --exclude=".git" --exclude=".idea" ./ root@95.216.223.67:/root/nechat_bot
-ssh root@95.216.223.67 "docker container rm nechat-bot --force && docker image rm nechat-bot && docker build -t nechat-bot /root/nechat_bot && docker run --restart on-failure -d --name nechat-bot nechat-bot"
+rsync -av -e ssh --exclude="node_modules" --exclude=".git" --exclude=".idea" --exclude=".DS_Store" ./ root@95.216.223.67:/root/nechat_bot
+ssh root@95.216.223.67 "docker container rm nechat-bot --force && docker build -t nechat-bot /root/nechat_bot && docker run --restart on-failure -d --name nechat-bot nechat-bot && docker image prune -f"
